3f78012fd92c0fee14ff55df0962962b4c7f89f4
BuildTools <unconfigured@null.spigotmc.org>
2023-06-30 02:58:18 -0400

    Heal to max health after death

--- src/main/java/com/whitehallplugins/reapermod/events/playerRespawnCopyDataCallback.java
+++ src/main/java/com/whitehallplugins/reapermod/events/playerRespawnCopyDataCallback.java
@@ -12,5 +12,6 @@ public class playerRespawnCopyDataCallback implements CopyFrom {
     @Override
     public void copyFromPlayer(ServerPlayerEntity oldPlayer, ServerPlayerEntity newPlayer, boolean alive) {
         Objects.requireNonNull(newPlayer.getAttributeInstance(EntityAttributes.GENERIC_MAX_HEALTH)).setBaseValue(oldPlayer.getAttributeValue(EntityAttributes.GENERIC_MAX_HEALTH));
+        newPlayer.setHealth(newPlayer.getMaxHealth());
     }
 }
